0602ff53b3ee0511d76550692e84f6e473bf7595
ovarunendra <vpsingh016@gmail.com>
2018-08-23 14:10:02 +0530

    Added redux form

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,37 +1,64 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { addBook } from '../actions'
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { addBook } from '../actions';
 
-class AddBook extends Component {
-  constructor(props){
+class AddBookForm extends Component {
+  constructor(props) {
     super(props);
-    this.state = {
-      name: '',
-      genre: '',
-    };
   }
-  addBook = () => {
-    const { name } = this.state;
-  	name && this.props.addBook(name);
-  }
+  addBook = (data) => {
+    this.props.addBook(data.name);
+    this.props.reset();
+  };
+
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <div id="add-book" className="form">
-        <div className="field">
-          <label>Book Name:</label>
-          <input type="text" onChange={(e) => this.setState({name: e.target.value})} />
-        </div>
-
-        <div className="field">
-          <label>Genre:</label>
-          <input type="text" onChange={(e) => this.setState({genre: e.target.value})}/>
-        </div>
-
-        <button onClick={() => this.addBook()}>+</button>
-      </div>
+      <form onSubmit={handleSubmit(this.addBook)} id="add-book" className="form">
+        <Field name="name" label="Book Name:" component={RenderInput} />
+        <button type="submit">+</button>
+      </form>
     );
   }
 }
+
+const validate = values => {
+  const errors = {};
+  if (!values.name) {
+    errors.name = 'Required';
+  }
+  return errors;
+}
+
+const createRenderer = render => ({ input, meta, label, ...rest }) =>
+  <div
+    className={[
+      "field",
+      meta.error && meta.touched ? 'error' : '',
+      meta.active ? 'active' : ''
+    ].join(' ')}
+  >
+    <label>
+      {label}
+    </label>
+    {render(input, label, rest)}
+    {meta.error &&
+      meta.touched &&
+      <span>
+        {meta.error}
+      </span>}
+  </div>
+
+const RenderInput = createRenderer((input, label) =>
+  <input {...input} placeholder={label} />
+)
+
+const AddBookReduxForm = reduxForm({
+  form: 'addbook',
+  destroyOnUnmount: true,
+  validate
+})(AddBookForm);
 
 const mapDispatchToProps = dispatch => ({
   addBook: name => dispatch(addBook(name))
@@ -40,4 +67,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   null,
   mapDispatchToProps
-)(AddBook);
+)(AddBookReduxForm);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import cache, { getBook } from './cache';
+import { reducer as reduxFormReducer } from 'redux-form';
 
 import {
   createList as createBookList,
@@ -13,6 +14,7 @@ import {
 // })
 
 export default combineReducers({
+  form: reduxFormReducer,
   cache,
   bookList: createBookList()
 })
