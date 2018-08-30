3391faa3dcbe1d10e7f317d014bf3a53ed11834d
ovarunendra <vpsingh016@gmail.com>
2018-08-30 11:49:44 +0530

    added more e2e test

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,15 +1,24 @@
 describe('Test', () => {
   beforeEach(() => {
-    cy.server();
     cy.visit('http://localhost:8080/');
   });
 
   context('When page is initially opened', function() {
+    it('should have title as "Support App"', () => {
+      cy.title().should('eq', 'Support App');
+    });
     it('should have heading as "Reading List"', function() {
       cy.get('h1').should('have.text', 'Reading List');
     });
+    it('should show "No Book Selected"', () => {
+      cy.get('#book-details > div').should('have.text', 'No Book Selected');
+    });
     it('should render book list', () => {
       cy.get('li').should('have.length', 2);
+    });
+    it('should have add button', () => {
+      cy.get('button').should('have.length', 1);
+      cy.get('button').should('have.text', '+');
     });
   });
 });
